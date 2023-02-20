521bdca85aa5138a66be245b15382007018561f2
GuntisBogotais221RDB075 <125827057+GuntisBogotais221RDB075@users.noreply.github.com>
2023-02-20 02:31:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,29 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i+1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
             pass
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
+
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
