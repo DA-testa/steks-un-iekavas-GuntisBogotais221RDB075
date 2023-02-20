fb196b80902e8f572036876fc5099c963f3e0505
GuntisBogotais221RDB075 <125827057+GuntisBogotais221RDB075@users.noreply.github.com>
2023-02-20 13:29:10 +0200

    Revert "Feedback"

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     print(mismatch)
