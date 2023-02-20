2ffbd5d20b0147c0111d81dc74457424672644eb
GuntisBogotais221RDB075 <125827057+GuntisBogotais221RDB075@users.noreply.github.com>
2023-02-20 13:30:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def find_mismatch(text):
 
 def main():
     text = input()
+    text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     print(mismatch)
